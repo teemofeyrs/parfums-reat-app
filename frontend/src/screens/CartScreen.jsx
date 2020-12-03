6d11834e33b4b79be92f41a6ce89cbb23f83e475
teemofeyrs <teemofeyrs@gmail.com>
2020-12-03 22:24:01 +0200

    add to cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {addToCart} from "../redux/reducers/cartReducer";
+import {useDispatch} from "react-redux";
 
 const CartScreen = (props) => {
     const productId = props.match.params.id
     const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+    },[productId, qty, dispatch])
     return(
         <div>
             <h1>Cart Screen</h1>
